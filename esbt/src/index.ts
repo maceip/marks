4607d7f417e25ba8786eafb4a72c9e0e533775a6
mac <ryan.macarthur@gmail.com>
2026-08-21 18:28:37 -0400

    Add @marks/esbt package skeleton

---
+++ esbt/src/index.ts
@@ -0,0 +1,7 @@
+export { EsbtDoc } from './doc.js';
+export type { EsbtConfig, EsbtEvent, EsbtExportOptions } from './doc.js';
+export { VersionVector } from './vector.js';
+export { UndoManager } from './undo.js';
+export { EphemeralStore } from './ephemeral.js';
+export { cmpWeight, newseq, Allocator } from './weight.js';
+export type { SiteId, Weight } from './weight.js';
